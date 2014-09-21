cc66036d79e34ba8462e6caff379f5e705c99de4
simonpai <simonpai@potix.com>
2014-09-21 15:20:16 +0800

    Close #20.

--- js/main.js
+++ js/main.js
@@ -17,6 +17,33 @@ $(function () {
 
 })(this);
 
+// scrolling //
+(function (window) {
+
+'use strict';
+var $ = window.jQuery,
+	scrolling = false,
+	i = 0;
+
+function setScrolling(value) {
+	if (scrolling == value)
+		return;
+	window.scrolling = scrolling = value;
+	$(window.document.body)[value ? 'addClass' : 'removeClass']('scrolling');
+}
+
+setScrolling(false);
+
+$(window.document).on('scroll', function () {
+	setScrolling(true);
+	i++;
+	setTimeout(function () {
+		if (!--i)
+			setScrolling(false);
+	}, window.scrollingCoolDown || 300);
+});
+
+})(this);
 
 
 // main //
