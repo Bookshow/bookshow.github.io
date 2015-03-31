101fb05449274f6816da2120cdcb6ef1f89e8ab7
simonpai <simonpai@potix.com>
2015-03-31 11:54:36 +0800

    Fine tune pricing JS.

--- js/pricing.js
+++ js/pricing.js
@@ -4,14 +4,16 @@
 var $ = window.jQuery;
 
 var PricingModel = function () {
+	this.criteria = {};
 	this._total = 0;
 };
 window.inherit(PricingModel, window.DataModel);
 
 PricingModel.prototype.recalculate = function () {
-	var x = (this.a ? 1000 : 0) + (this.b ? 2000 : 0) + (this.c ? 3000 : 0),
-		y = 1 + (this.d ? -0.1 : 0) + (this.e ? -0.2 : 0) + (this.f ? -0.3 : 0),
-		z = (this.g ? 100 : 0) + (this.h ? 200 : 0) + (this.i ? 300 : 0),
+	var cr = this.criteria,
+		x = (cr.a ? 1000 : 0) + (cr.b ? 2000 : 0) + (cr.c ? 3000 : 0),
+		y = 1 + (cr.d ? -0.1 : 0) + (cr.e ? -0.2 : 0) + (cr.f ? -0.3 : 0),
+		z = (cr.g ? 100 : 0) + (cr.h ? 200 : 0) + (cr.i ? 300 : 0),
 		total = x * y + z;
 	if (total == this._total)
 		return;
@@ -19,42 +21,42 @@ PricingModel.prototype.recalculate = function () {
 };
 
 PricingModel.prototype.toggle = function (key) {
-	this.set(key, !this[key]);
+	this.set(key, !this.criteria[key]);
 };
 
 PricingModel.prototype.set = function (key, value) {
-	if (value == this[key])
+	if (value == this.criteria[key])
 		return;
-	this[key] = value;
+	this.criteria[key] = value;
 	this.trigger('criterion', { key: key, value: value });
 	//this.recalculate();
 };
 
 $(function () {
-	var model = new PricingModel(),
+	var model = window.pricingModel = new PricingModel(),
 		elemMap = {},
 		totalElem = $('#pricing .u-total')[0];
 	
 	$('#pricing .u-criterion').each(function () {
 		elemMap[$(this).data('value')] = this;
 	});
-
-	$('#pricing').on('click', '.u-criterion', function () {
+	
+	$('#pricing')
+	.on('click', '.u-criterion', function () {
 		model.toggle($(this).data('value'));
-	});
-
-	$('#pricing').on('click', '.u-submit', function () {
+	})
+	.on('click', '.u-submit', function () {
 		model.recalculate();
 	});
-
-	model.on('criterion', function (data) {
+	
+	model
+	.on('criterion', function (data) {
 		var elem = elemMap[data.key];
 		if (!elem)
 			return;
 		$(elem)[data.value ? 'addClass' : 'removeClass']('selected');
-	});
-
-	model.on('total', function (value) {
+	})
+	.on('total', function (value) {
 		totalElem.innerHTML = '' + value + '元起';
 	});
 });
