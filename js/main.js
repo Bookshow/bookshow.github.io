5562a242a48a8344450656c92d15337fa133a9cd
simonpai <simonpai@potix.com>
2015-05-11 17:21:25 +0800

    Tab navigator functions.

--- js/main.js
+++ js/main.js
@@ -239,3 +239,38 @@ $(function () {
 });
 
 })(this);
+
+
+
+/* tab extension*/
+(function (window) {
+
+'use strict';
+var $ = window.jQuery;
+
+function showLI(li) {
+	if (!li)
+		return;
+	$(li.querySelector('[role="tab"]')).tab('show');
+}
+
+$(window.document).on('click', '[role="navigator"]', function (e) {
+	var elem = e.currentTarget,
+		$elem = $(elem),
+		type = $elem.data('toggle'),
+		target = $elem.data('target'); // TODO: or href
+	target = $(target)[0];
+	if (!target || !type)
+		return;
+	
+	var currentLI = target.querySelector('.active');
+	if (type == 'previous') {
+		showLI(currentLI && currentLI.previousElementSibling);
+		
+	} else if (type == 'next') {
+		showLI(currentLI && currentLI.nextElementSibling);
+	}
+	e.preventDefault();
+});
+
+})(this);
