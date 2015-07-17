021e8137ac07faacfd90bc7bef664ba425cea3fe
simonpai <simonpai@potix.com>
2015-07-17 16:05:42 +0800

    Index page heading animation.

--- _includes/js/index.js
+++ _includes/js/index.js
@@ -18,3 +18,50 @@ window.events.on('size', function () {
 });
 
 })(this);
+
+
+
+// scroll position //
+(function (window) {
+
+'use strict';
+var $ = window.jQuery,
+	$h1,
+	$win = $(window),
+	height,
+	headings,
+	init = false;
+
+window.events.on('size', function () {
+	if (!init) {
+		if (window.document.body.className.contains('index')) {
+			($h1 = $('h1')).addClass('out');
+			$(window.document).on('scroll', onScroll);
+		}
+		init = true;
+	}
+	if ($h1) {
+		headings = [];
+		height = $win.height();
+		$h1.each(function () {
+			headings.push({
+				element: this,
+				top: $(this).offset().top
+			});
+		});
+	}
+});
+
+function onScroll() {
+	var pos = $win.scrollTop() + height * 2 / 3,
+		i = 0, 
+		len = headings.length;
+	for (; i < len; i++)
+		if (headings[i].top > pos)
+			break;
+	for (var j = 0; j < i; j++)
+		$(headings[j].element).removeClass('out');
+	headings.splice(0, i);
+}
+
+})(this);
